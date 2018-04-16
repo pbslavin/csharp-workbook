a800005136b482c7a8d6898ace618fb7f9fb78cd
Peter Slavin <pbslavin@gmail.com>
2018-04-16 14:47:44 -0500

    added FizzBuzz

--- FizzBuzz/Program.cs
+++ FizzBuzz/Program.cs
@@ -4,9 +4,39 @@ namespace FizzBuzz
 {
     class Program
     {
-        static void Main(string[] args)
+        static bool isNumeric = false;
+        static void Main()
         {
-            Console.WriteLine("Hello World!");
+            while (!isNumeric) {
+                Console.WriteLine("Please enter a natural number up to 100.: ");
+                string numString = Console.ReadLine();
+                isNumeric = Int32.TryParse(numString, out int n);
+                if (isNumeric)
+                {   
+                    FizzBuzzIt(n);
+                } else {
+                    Console.WriteLine("That won't cut it.\n");
+                }
+            }
+        }
+        static void FizzBuzzIt(int n) 
+        {   
+            Console.WriteLine();
+            for (int i = 1; i < n + 1; i ++) 
+            {
+                if ((i % 5 == 0) && (i % 3 == 0)) {
+                    Console.WriteLine("fizz buzz");
+                } else if (i % 3 == 0)
+                {
+                    Console.WriteLine("fizz");
+                } else if (i % 5 == 0) 
+                {
+                    Console.WriteLine("buzz");
+                } else {
+                    Console.WriteLine(i);
+                }
+            }
+            Console.WriteLine();
         }
     }
 }

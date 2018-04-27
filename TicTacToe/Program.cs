d64cac384ded5bffb15c3ed9f3bbede4c8820336
Peter Slavin <pbslavin@gmail.com>
2018-04-27 00:04:14 -0500

    refactored TicTacToe to check wins by looping

--- TicTacToe/Program.cs
+++ TicTacToe/Program.cs
@@ -38,28 +38,83 @@ namespace TicTacToe
         }
 
         public static void GetInput()
-        {   
-            bool invalid = true;
-            while (invalid)
-            {
+        {    
+            int row = 0;
+            int column = 0;
+            bool invalidRow = false;
+            bool invalidColumn = false;
+            bool occupied;
+            do
+            {   
+                occupied = false;
                 Console.WriteLine("Player " + playerTurn);
-                Console.WriteLine("Enter Row:");
-                int row = int.Parse(Console.ReadLine());
-                Console.WriteLine("Enter Column:");
-                int column = int.Parse(Console.ReadLine());
+
+                // take row input and check if row is valid
+                do
+                {
+                    Console.WriteLine("Enter Row:");
+                    try
+                    {
+                        row = int.Parse(Console.ReadLine());
+                    }
+                    catch
+                    {
+                        invalidRow = true;
+                        Console.WriteLine("That entry is invalid");
+                        continue;
+                    }
+                    if (row > -1 && row < 3)
+                    {
+                        invalidRow = false;
+                    }
+                    else
+                    {
+                        invalidRow = true;
+                        Console.WriteLine("That entry is invalid");
+                    }
+                }
+                while (invalidRow);
+
+                // take row input and check if row is valid
+                do
+                {
+                    Console.WriteLine("Enter Column:");
+                    try
+                    {
+                        column = int.Parse(Console.ReadLine());
+                    }
+                    catch
+                    {
+                        invalidColumn = true;
+                        Console.WriteLine("That entry is invalid");
+                        continue;
+                    }
+                    if (column > -1 && column < 3)
+                    {
+                        invalidColumn = false;
+                    }
+                    else
+                    {
+                        invalidColumn = true;
+                        Console.WriteLine("That entry is invalid");
+                    }
+                }
+                while (invalidColumn);
+                
                 
                 // Check if cell is occupied
                 if (board[row][column] != " ") 
                 {
                     Console.WriteLine("That cell is occupied.");
-                    invalid = true;
+                    occupied = true;
                 }
                 else
                 {
-                    invalid = false;
+                    occupied = false;
                     PlaceMark(row, column);
                 }
-            }
+            } 
+            while (occupied);
         }
 
         public static void PlaceMark(int row, int column)
@@ -74,23 +129,68 @@ namespace TicTacToe
         }
         
         public static bool HorizontalWin()
-        {
-            return ((board[0][0] == playerTurn && board[0][1] == playerTurn && board[0][2] == playerTurn) || 
-            (board[1][0] == playerTurn && board[1][1] == playerTurn && board[1][2] == playerTurn) ||
-            (board[2][0] == playerTurn && board[2][1] == playerTurn && board[2][2] == playerTurn));
+        {   
+            for (int i = 0; i < 3; i ++)
+            {
+                for (int j = 1; j < 3; j ++)
+                {
+                    if (board[i][j] != board[i][0] || board[i][j] == " ")
+                    {
+                        break;
+                    }
+                    else if (j == 2)
+                    {
+                        return true;
+                    }
+                }
+            }
+            return false;
         }
 
         public static bool VerticalWin()
         {
-            return ((board[0][0] == playerTurn && board[1][0] == playerTurn && board[2][0] == playerTurn) || 
-            (board[0][1] == playerTurn && board[1][1] == playerTurn && board[2][1] == playerTurn) ||
-            (board[0][2] == playerTurn && board[1][2] == playerTurn && board[2][2] == playerTurn));
+            for (int i = 0; i < 3; i ++)
+            {
+                for (int j = 1; j < 3; j ++)
+                {
+                    if (board[j][i] != board[0][i] || board[j][i] == " ")
+                    {
+                        break;
+                    }
+                    else if (j == 2)
+                    {
+                        return true;
+                    }
+                }
+            }
+            return false;
         }
 
         public static bool DiagonalWin()
         {
-            return ((board[0][0] == playerTurn && board[1][1] == playerTurn && board[2][2] == playerTurn) || 
-            (board[0][2] == playerTurn && board[1][1] == playerTurn && board[2][0] == playerTurn));
+            for (int i = 0; i < 3; i ++)
+            {
+                if (board[i][i] != board[1][1] || board[i][i] == " ")
+                {
+                    break;
+                }
+                else if (i == 2)
+                    {
+                        return true;
+                    }
+            }
+            for (int i = 0; i < 3; i ++)
+            {
+                if (board[i][2 - i] != board[1][1] || board[i][2 - i] == " ")
+                {
+                    break;
+                }
+                else if (i == 2)
+                {
+                    return true;
+                }
+            }
+            return false;
         }
 
         public static void DrawBoard()

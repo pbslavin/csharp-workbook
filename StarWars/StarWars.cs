c1613c9dad48e9eca12fa86dc2349637e34c4d0c
Peter Slavin <pbslavin@gmail.com>
2018-05-10 19:36:52 -0500

    turned Towers winFlag into a property, and took class declarations out of Program class

---
+++ StarWars/StarWars.cs
@@ -0,0 +1,88 @@
+﻿using System;
+
+namespace HelloWorld
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Person leia = new Person("Leia", "Organa", "Rebel");
+            Person darth = new Person("Darth", "Vader", "Imperial");
+            Ship falcon = new Ship("Rebel", "smuggling", 2);
+            Ship tie = new Ship("Imperial", "fighter", 1);
+            Console.WriteLine("");
+
+        }
+    }
+
+     class Person 
+    {
+        private string firstName;
+        private string lastName;
+        private string alliance;
+        public Person(string firstName, string lastName, string alliance)
+        {
+            this.firstName = firstName;
+            this.lastName = lastName;
+            this.alliance = alliance;
+        }
+
+        public string FullName
+        {
+            get
+            {
+                return this.firstName + " " + this.lastName;
+            }
+
+            set
+            {
+                string [] names = value.Split(' ');
+                this.firstName = names[0];
+                this.lastName = names[1];
+            }
+        }
+    }
+
+    class Ship
+    {
+        private Person[] passengers;
+
+        public Ship(string type, string alliance, int size)
+        {
+            this.Type = type;
+            this.Alliance = alliance;
+            this.passengers = new Person[size];
+        }
+
+        public string Type { get; set; }
+
+        public string Alliance { get; set; }
+
+        public string Passengers 
+        {
+            get
+            {
+                string names = "Passenger list: ";
+                foreach (var person in passengers)
+                {
+                    if (person != null)
+                    {
+                        names += person.FullName + " ";
+                    }
+                }
+                return names;
+                
+            }
+        }
+
+        public void EnterShip(Person person, int seat)
+        {
+            this.passengers[seat] = person;
+        }
+
+        public void ExitShip(int seat)
+        {
+            this.passengers[seat] = null;
+        }
+    }
+}
--- TowersOfHanoi/TowersOfHanoi.cs
+++ TowersOfHanoi/TowersOfHanoi.cs
@@ -4,6 +4,7 @@ namespace TowersOfHanoi
 {
     class Program
     {
+
         static void Main(string[] args)
         {
             Disc five = new Disc(5);
@@ -22,7 +23,7 @@ namespace TowersOfHanoi
             rodTwo.reportDiscs();
             rodThree.reportDiscs();
             Console.WriteLine("");
-            while (rodOne.winFlag == false && rodTwo.winFlag == false && rodThree.winFlag == false)
+            while (rodOne.WinFlag == false && rodTwo.WinFlag == false && rodThree.WinFlag == false)
             {
                 Console.WriteLine("Move from? (Enter 1, 2 or 3)");
                 string from = Console.ReadLine();
@@ -48,7 +49,7 @@ namespace TowersOfHanoi
                 }
                 else
                 {
-                    rodOne.printErrorMessage();        
+                    Rod.printErrorMessage();        
                 }
                 rodOne.reportDiscs();
                 rodTwo.reportDiscs();
@@ -56,113 +57,117 @@ namespace TowersOfHanoi
                 Console.WriteLine("");
             }
         }
+    }
 
-        class Rod 
+    class Rod 
+    {
+        // public bool winFlag = false;
+
+        public static void printErrorMessage()
         {
-            public bool winFlag = false;
-
-            public Rod(Disc[] initialStack, string num)
-            {
-                Number = num;
-                Stack = initialStack;
-            }
-
-            public string Number { get; private set; }
-
-            public Disc[] Stack { get; private set; }
-
-            public void checkAndMove(Rod dest)
-            { 
-                if (this.Stack[0] != null)
-                {
-                    Disc md = this.MoveDisc(dest);
-                    if (md != null)
-                    {
-                        Console.WriteLine($"\nMoved size {md.Size} disc to rod {dest.Number}\n");
-                        if (dest.checkForWin())
-                        {
-                            dest.winFlag = true;
-                        }
-                        return;
-                    }
-                }
-                printErrorMessage(); 
-            }
-
-            private Disc MoveDisc(Rod dest)
-            {
-                Disc moved = new Disc(0);
-                int i;
-                for (i = Stack.Length - 1; i > -1; i --)
-                {
-                    if (this.Stack[i] != null)
-                    {
-                        moved = this.Stack[i];
-                        this.Stack[i] = null;
-                        break;
-                    }
-                }
-                if (dest.Stack[0] != null)
-                {
-                    for (int j = this.Stack.Length - 1; j > -1; j --)
-                    {
-                        if ((dest.Stack[j] != null && dest.Stack[j].Size > moved.Size))
-                        {
-                            return dest.Stack[j + 1] = moved;
-                        }
-                    }
-                }
-                else
-                {
-                    return dest.Stack[0] = moved;
-                }
-                
-                // replace disc in its original position if move is invalid
-                this.Stack[i] = moved;
-                return null;
-            }
-
-            public void reportDiscs()
-            {   
-                Console.Write($"Rod {this.Number}: ");
-                if (this.Stack[0] != null)
-                {
-                    foreach (Disc disc in this.Stack)
-                    {
-                        if (disc != null)
-                        {
-                            Console.Write($"{disc.Size} ");
-                        }
-                    }
-                }
-            Console.WriteLine("");
-            }
-
-            public void printErrorMessage()
-            {
-                Console.WriteLine("\nTry again!\n");
-            }
-
-            public bool checkForWin()
-            {
-                if (this.Stack[this.Stack.Length - 1] != null)
-                {
-                    Console.WriteLine("You win!\n");
-                    return true;
-                }
-                return false;
-            }
+            Console.WriteLine("\nTry again!\n");
         }
 
-        class Disc 
+        public Rod(Disc[] initialStack, string num)
         {
-            public Disc(int size)
-            {
-                Size = size;
-            }
-
-            public int Size { get; private set; }
-
+            Number = num;
+            Stack = initialStack;
+            WinFlag = false;
         }
+
+        public string Number { get; private set; }
+
+        public Disc[] Stack { get; private set; }
+
+        public bool WinFlag { get; private set; }
+
+        public void checkAndMove(Rod dest)
+        { 
+            if (this.Stack[0] != null)
+            {
+                Disc md = this.MoveDisc(dest);
+                if (md != null)
+                {
+                    Console.WriteLine($"\nMoved size {md.Size} disc to rod {dest.Number}\n");
+                    if (dest.checkForWin())
+                    {
+                        dest.WinFlag = true;
+                    }
+                    return;
+                }
+            }
+            printErrorMessage(); 
+        }
+
+        private Disc MoveDisc(Rod dest)
+        {
+            Disc moved = new Disc(0);
+            int i;
+            for (i = Stack.Length - 1; i > -1; i --)
+            {
+                if (this.Stack[i] != null)
+                {
+                    moved = this.Stack[i];
+                    this.Stack[i] = null;
+                    break;
+                }
+            }
+            if (dest.Stack[0] != null)
+            {
+                for (int j = this.Stack.Length - 1; j > -1; j --)
+                {
+                    if ((dest.Stack[j] != null && dest.Stack[j].Size > moved.Size))
+                    {
+                        return dest.Stack[j + 1] = moved;
+                    }
+                }
+            }
+            else
+            {
+                return dest.Stack[0] = moved;
+            }
+            
+            // replace disc in its original position if move is invalid
+            this.Stack[i] = moved;
+            return null;
+        }
+
+        public void reportDiscs()
+        {   
+            Console.Write($"Rod {this.Number}: ");
+            if (this.Stack[0] != null)
+            {
+                foreach (Disc disc in this.Stack)
+                {
+                    if (disc != null)
+                    {
+                        Console.Write($"{disc.Size} ");
+                    }
+                }
+            }
+        Console.WriteLine("");
+        }
+
+
+        public bool checkForWin()
+        {
+            if (this.Stack[this.Stack.Length - 1] != null)
+            {
+                Console.WriteLine("You win!\n");
+                return true;
+            }
+            return false;
+        }
+    }
+
+    class Disc 
+    {
+        public Disc(int size)
+        {
+            Size = size;
+        }
+
+        public int Size { get; private set; }
+
     }
 }

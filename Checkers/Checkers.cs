5d41e81f9539d085b60ee9700ff1e7edd04d7485
Peter Slavin <pbslavin@gmail.com>
2018-05-29 23:22:49 -0500

    Checkers complete, with kinging

--- Checkers/Checkers.cs
+++ Checkers/Checkers.cs
@@ -8,10 +8,12 @@ namespace Checkers
     public class Checker
     {
         private string symbol;
+        private bool king;
         public Checker(string color, int[] position)
         {
             Color = color;
             Position = position;
+            king = false;
             if (Color == "white")
             {
                 int openCircleId = int.Parse("25CB", System.Globalization.NumberStyles.HexNumber);
@@ -23,6 +25,15 @@ namespace Checkers
                 int closedCircleId = int.Parse("25CF", System.Globalization.NumberStyles.HexNumber);
                 string closedCircle = char.ConvertFromUtf32(closedCircleId);
                 symbol = closedCircle;
+            }
+        }
+        public bool King
+        {
+            get {
+                return king;
+            }
+            set {
+                king = value;
             }
         }
         public string Symbol
@@ -43,12 +54,6 @@ namespace Checkers
         private List<Checker> checkers = new List<Checker>();
         public static int[,] whitePositions = new int[12,2];
         public static int[,] blackPositions = new int[12,2];
-        
-        public Board()
-        {
-
-            return;
-        }
         public string[][] Grid  { get; private set; }
         public List<Checker> Checkers
         {
@@ -181,86 +186,74 @@ namespace Checkers
         {
             if (Checkers.Find(x => x.Position.SequenceEqual(
                 new int[] { row, column })) == null)
-            {        
+            {      
+                // jumping  
                 if ((Math.Abs(row - checker.Position[0]) == 2 && 
-                    Math.Abs(column - checker.Position[1]) == 2) // diagonal jump
-                    // || (Math.Abs(row - checker.Position[0]) == 2 && 
-                    // Math.Abs(column - checker.Position[1]) == 0) || // up or down jump (king)
-                    // (Math.Abs(row - checker.Position[0]) == 0 && 
-                    // Math.Abs(column - checker.Position[1]) == 2) // left or right jump (king)
-                    )   
+                    Math.Abs(column - checker.Position[1]) == 2))  
                 {
-                    // straight up (king)
-                    // if (row - checker.Position[0] == -2 && 
-                    //     column - checker.Position[1] == 0)
-                    // {
-                    //     Checker jumpedChecker = Checkers.Find(x => x.Position.SequenceEqual(
-                    //         new int[] { row + 1, column }));
-                    //     if (jumpedChecker != null)
-                    //     {
-                    //         Checkers.Remove(Checkers.Find(x => x.Position.SequenceEqual(
-                    //         new int[] { row + 1, column })));
-                    //     }
-                    //     else
-                    //     {
-                    //         Console.WriteLine("Invalid move");
-                    //         return;
-                    //     }
-                    // }
-                    // straight down (king)
-                    // else if (row - checker.Position[0] == 2 && 
-                    //     column - checker.Position[1] == 0)
-                    // {
-                    //     Checkers.Remove(Checkers.Find(x => x.Position.SequenceEqual(
-                    //         new int[] { row - 1, column })));
-                    // }
-                    // straight right (king)
-                    // else if (row - checker.Position[0] == 0 && 
-                    //     column - checker.Position[1] == 2)
-                    // {
-                    //     Checkers.Remove(Checkers.Find(x => x.Position.SequenceEqual(
-                    //         new int[] { row, column - 1 })));
-                    // }
-                    //straight left (king)
-                    // else if (row - checker.Position[0] == 0 && 
-                    //     column - checker.Position[1] == -2)
-                    // {
-                    //     Checkers.Remove(Checkers.Find(x => x.Position.SequenceEqual(
-                    //         new int[] { row, column + 1 })));
-                    // }
-                    //diagonal up right
-                    if (row - checker.Position[0] == -2 && 
+                    if (checker.Color == "black" || checker.King)
+                    {
+                        // up right
+                        if (row - checker.Position[0] == -2 && 
                         column - checker.Position[1] == 2)
-                    {
-                        JumpChecker(checker, row + 1, column - 1, row, column);
-                        return;
+                        {
+                            JumpChecker(checker, row + 1, column - 1, row, column);
+                            KingCheck(checker, row);
+                            return;
+                        }
+                        // up left
+                        if (row - checker.Position[0] == -2 && 
+                            column - checker.Position[1] == -2)
+                        {
+                            JumpChecker(checker, row + 1, column + 1, row, column);
+                            KingCheck(checker, row);
+                            return;
+                        }
+                        else
+                        {
+                            Console.WriteLine("Invalid move");
+                            return;
+                        }
                     }
-                    //diagonal up left
-                    if (row - checker.Position[0] == -2 && 
-                        column - checker.Position[1] == -2)
+                    if (checker.Color == "white" || checker.King)
                     {
-                        JumpChecker(checker, row + 1, column + 1, row, column);
-                        return;
+                        // down right
+                        if (row - checker.Position[0] == 2 && 
+                            column - checker.Position[1] == 2)
+                        {
+                            JumpChecker(checker, row - 1, column - 1, row, column);
+                            KingCheck(checker, row);
+                            return;
+                        }
+                        // down left
+                        if (row - checker.Position[0] == 2 && 
+                            column - checker.Position[1] == -2)
+                        {
+                            JumpChecker(checker, row - 1, column + 1, row, column);
+                            KingCheck(checker, row);
+                            return;      
+                        }
+                        else
+                        {
+                            Console.WriteLine("Invalid move");
+                        }
                     }
-                    //diagonal down right
-                    if (row - checker.Position[0] == 2 && 
-                        column - checker.Position[1] == 2)
-                    {
-                        JumpChecker(checker, row - 1, column - 1, row, column);
-                        return;
-                    }
-                    //diagonal down left
-                    if (row - checker.Position[0] == 2 && 
-                        column - checker.Position[1] == -2)
-                    {
-                        JumpChecker(checker, row - 1, column + 1, row, column);
-                        return;      
-                    }
+                    
                 }
                 else if (Math.Abs(row - checker.Position[0]) == 1 && 
                     Math.Abs(column - checker.Position[1]) == 1)
                 {
-                    PlaceChecker(checker, row, column);
+                    if ((row > checker.Position[0] && checker.Color == "white") ||
+                        (row < checker.Position[0] && checker.Color == "black") ||
+                        checker.King)
+                    {
+                        PlaceChecker(checker, row, column);
+                        KingCheck(checker, row);
+                    }
+                    else
+                    {
+                        Console.WriteLine("Invalid move");
+                    }
                     return;
                 }
                 else
@@ -299,11 +292,32 @@ namespace Checkers
                 new int[] { row, column })));
             return;
         }
+
+        public void KingCheck(Checker checker, int row)
+        {
+            if ((checker.Color == "white" && row == 8) ||
+                (checker.Color == "black" && row == 1))
+            {
+                checker.King = true;
+                if (checker.Color == "white")
+                {
+                    int openDiamondId = int.Parse("25C7", System.Globalization.NumberStyles.HexNumber);
+                    string openDiamond = char.ConvertFromUtf32(openDiamondId);
+                    checker.Symbol = openDiamond;
+                }
+                else
+                {
+                    int closedDiamondId = int.Parse("25C6", System.Globalization.NumberStyles.HexNumber);
+                    string closedDiamond = char.ConvertFromUtf32(closedDiamondId);
+                    checker.Symbol = closedDiamond;
+                }
+            }
+        }
         
         public bool CheckForWin()
         {
             return Checkers.All(x => x.Color == "white") || 
-                !Checkers.Exists(x => x.Color == "black");
+                !Checkers.Exists(x => x.Color == "white");
         }
     }
 
@@ -374,3 +388,47 @@ namespace Checkers
         }
     }
 }
+
+                    // || (Math.Abs(row - checker.Position[0]) == 2 && 
+                    // Math.Abs(column - checker.Position[1]) == 0) || // up or down jump (king)
+                    // (Math.Abs(row - checker.Position[0]) == 0 && 
+                    // Math.Abs(column - checker.Position[1]) == 2) // left or right jump (king)  
+
+                    // straight up (king)
+                    // if (row - checker.Position[0] == -2 && 
+                    //     column - checker.Position[1] == 0)
+                    // {
+                    //     Checker jumpedChecker = Checkers.Find(x => x.Position.SequenceEqual(
+                    //         new int[] { row + 1, column }));
+                    //     if (jumpedChecker != null)
+                    //     {
+                    //         Checkers.Remove(Checkers.Find(x => x.Position.SequenceEqual(
+                    //         new int[] { row + 1, column })));
+                    //     }
+                    //     else
+                    //     {
+                    //         Console.WriteLine("Invalid move");
+                    //         return;
+                    //     }
+                    // }
+                    // straight down (king)
+                    // else if (row - checker.Position[0] == 2 && 
+                    //     column - checker.Position[1] == 0)
+                    // {
+                    //     Checkers.Remove(Checkers.Find(x => x.Position.SequenceEqual(
+                    //         new int[] { row - 1, column })));
+                    // }
+                    // straight right (king)
+                    // else if (row - checker.Position[0] == 0 && 
+                    //     column - checker.Position[1] == 2)
+                    // {
+                    //     Checkers.Remove(Checkers.Find(x => x.Position.SequenceEqual(
+                    //         new int[] { row, column - 1 })));
+                    // }
+                    //straight left (king)
+                    // else if (row - checker.Position[0] == 0 && 
+                    //     column - checker.Position[1] == -2)
+                    // {
+                    //     Checkers.Remove(Checkers.Find(x => x.Position.SequenceEqual(
+                    //         new int[] { row, column + 1 })));
+                    // }

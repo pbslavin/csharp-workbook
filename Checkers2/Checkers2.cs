f228df56bd6f744eaaa7daa12aa3be01a2e51b37
Peter Slavin <pbslavin@gmail.com>
2018-07-09 19:59:02 -0500

    Changed HelloWorld to name/age/birthyear calculation app.

---
+++ Checkers2/Checkers2.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Checkers2
+{
+    // class Checker()
+    // {
+    //     public string Symbol { get; set; }
+    //     public int[] Position { get; set; }
+    //     public string Color { get; set; }
+    // }
+
+    // class Board()
+    // {
+    //     public string[][] Grid { get; set; }
+    //     public List<Checker> Checkers { get; set; }
+    //     static void CreateBoard()
+    //     {
+
+    //     }
+    //     void DrawBoard()
+    //     {
+
+    //     }
+    //     void GenerateBoard()
+    //     {
+
+    //     }
+    //     void SelectChecker()
+    //     {
+
+    //     }
+    //     void RemoveChecker()
+    //     {
+
+    //     }
+    //     bool CheckForWin()
+    //     {
+
+    //     }
+    // }
+
+    // class Game()
+    // {
+    //     static void Start()
+    //     {
+
+    //     }
+    // }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            int openCircleId = int.Parse("25CB", System.Globalization.NumberStyles.HexNumber);
+            string openCircle = char.ConvertFromUtf32(0x10FFFC);
+
+            Console.WriteLine($"open circle: {openCircle}");
+
+            int closedCircleId = int.Parse("25CF", System.Globalization.NumberStyles.HexNumber);
+            string closedCircle = char.ConvertFromUtf32(closedCircleId);
+
+            Console.WriteLine($"closed circle: {closedCircleId}");
+        }
+    }
+}
--- HelloWorld/HelloWorld.cs
+++ HelloWorld/HelloWorld.cs
@@ -1,12 +1,36 @@
 ﻿using System;
-
-namespace HelloWorld
+					
+public class Program
 {
-    class Program
-    {
-        static void Main(string[] args)
+	public static void Main()
+	{
+		string name = "";
+        string ageString = "";
+		int age = 0;
+        string yearString = "";
+		int year = 0;
+		
+		Console.WriteLine("Please enter your name: ");
+		name = Console.ReadLine();
+		Console.WriteLine("Please enter your age: ");
+        ageString = Console.ReadLine();
+        bool validAge = int.TryParse(ageString, out age);
+        while (!validAge)
         {
-            Console.WriteLine("Hello World!");
+           Console.WriteLine("Please enter a valid age.");
+           ageString = Console.ReadLine();
+           validAge = int.TryParse(ageString, out age);
         }
-    }
+		Console.WriteLine("Please enter the year: ");
+        yearString = Console.ReadLine();
+        bool validYear = int.TryParse(yearString, out year);
+        while (!validYear)
+        {
+            Console.WriteLine("Please enter a valid year.");
+            yearString = Console.ReadLine();
+            validYear = int.TryParse(yearString, out year);
+        }
+		
+		Console.WriteLine("Hello! My name is {0} and I am {1} years old. I was born in {2}.", name, age, year-age);
+	}
 }
